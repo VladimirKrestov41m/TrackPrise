5052ff4b1f76f95e585023980235f1e8ed2214f4
Vladimir.Krestov <lFreeZz.37l@gmail.com>
2018-12-16 13:44:25 +0300

    All added

---
+++ TP/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TP.Models;
+
+namespace TP.Controllers
+{
+    public class HomeController : Controller
+    {
+        // создаем контекст данных
+        ProductsContext db = new ProductsContext();
+
+        public ActionResult Index()
+        {
+            // получаем из бд все объекты Book
+            IEnumerable<Product> products = db.Products;
+            // передаем все объекты в динамическое свойство Books в ViewBag
+            ViewBag.Products = products;
+            // возвращаем представление
+            return View();
+        }
+    }
+}
